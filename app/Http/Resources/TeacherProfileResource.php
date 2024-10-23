cbe5bc92179384af668d97029b46ccfbfec4c8dc
Feras <ferasshaher2013@gmail.com>
2024-10-23 15:13:56 +0300

    teacher-description

--- app/Http/Resources/TeacherProfileResource.php
+++ app/Http/Resources/TeacherProfileResource.php
@@ -23,6 +23,7 @@ class TeacherProfileResource extends JsonResource
             'id'=>$this->id,
             'name'=>$this->name,
             'description'=>$lecturerSettingTranslation ?$lecturerSettingTranslation->description: $this->lecturerSetting->description,
+            'position'=>$lecturerSettingTranslation ?$lecturerSettingTranslation->position: $this->lecturerSetting->position,
             'image'=>imageUrl($this->image,'100x100'),
             'experience'=>$this->lecturerSetting?->exp_years,
             'mother_lang'=>$this->motherLang?->name,
