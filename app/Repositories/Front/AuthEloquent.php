9c056fd1f721aaf38efbe6a156fcaa085d2ebf6c
ahmed farrag <a7med97mu@gmail.com>
2024-11-03 23:15:12 +0200

    Working on files , clickup and logic edits notes

--- app/Repositories/Front/AuthEloquent.php
+++ app/Repositories/Front/AuthEloquent.php
@@ -320,15 +320,15 @@ class AuthEloquent extends HelperEloquent
         $user = $this->getUser($is_web);
 
         $diff_in_hours = diffInHours($user->last_send_validation_code, Carbon::now());
-        // if ($user->try_num_validation > 2 &&  $diff_in_hours == 0) {
+        if ($user->try_num_validation > 2 &&  $diff_in_hours == 0) {
 
-        //     return
-        //         [
-        //             'message' => __('message.unable_to_resend_try_again_in_an_hour'),
-        //             'alert_class' => 'alert-danger',
-        //             'status' => false,
-        //         ];
-        // }
+            return
+                [
+                    'message' => __('message.unable_to_resend_try_again_in_an_hour'),
+                    'alert_class' => 'alert-danger',
+                    'status' => false,
+                ];
+        }
 
         $user->sendVerificationCode();
         $user->try_num_validation = $user->try_num_validation + 1;
