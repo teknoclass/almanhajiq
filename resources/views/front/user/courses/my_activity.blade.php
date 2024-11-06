a8fbb5246f044c2dfbe088d4a3fb8a07a02d9346
ahmed farrag <a7med97mu@gmail.com>
2024-11-07 01:25:27 +0200

    files 9-10 requirements

--- resources/views/front/user/courses/my_activity.blade.php
+++ resources/views/front/user/courses/my_activity.blade.php
@@ -191,7 +191,7 @@
                                         <div class="icon-chevron me-1 d-flex"><i class="fa-solid fa-circle-chevron-left"></i></div>
                                     </div>
                                     <div class="col">
-                                        <a class="d-flex align-items-center col-auto" href="{{ route('user.courses.curriculum.openByItem', ['course_id'=>$course->id, 'type' => 'quiz', 'id'=>@$quiz->item->id]) }}">
+                                        <a class="d-flex align-items-center col-auto" @if(@$quiz->item) href="{{ route('user.courses.curriculum.openByItem', ['course_id'=>$course->id, 'type' => 'quiz', 'id'=>@$quiz->item->id]) }}" @endif>
                                             <p class="ms-2">{{ @$quiz->item->title }}</p>
                                         </a>
                                     </div>
