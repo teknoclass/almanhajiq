8d3bf44fa4898526e7e2af39ac5f3adff19e9666
Rajaei003 <rajaei.alsafadi.2003@gmail.com>
2024-11-07 16:35:08 +0300

    change on teacher resource

--- app/Http/Resources/TeacherProfileResource.php
+++ app/Http/Resources/TeacherProfileResource.php
@@ -22,11 +22,11 @@ class TeacherProfileResource extends JsonResource
         return [
             'id'=>$this->id,
             'name'=>$this->name,
-            'description'=>$lecturerSettingTranslation ?$lecturerSettingTranslation->description: $this->lecturerSetting->description,
-            'position'=>$lecturerSettingTranslation ?$lecturerSettingTranslation->position: $this->lecturerSetting->position,
+            'description'=>$lecturerSettingTranslation ?$lecturerSettingTranslation->description: @$this->lecturerSetting->description,
+            'position'=>$lecturerSettingTranslation ?$lecturerSettingTranslation->position: @$this->lecturerSetting->position,
             'image'=>imageUrl($this->image,'100x100'),
-            'experience'=>$this->lecturerSetting?->exp_years,
-            'mother_lang'=>$this->motherLang?->name,
+            'experience'=>@$this->lecturerSetting?->exp_years,
+            'mother_lang'=>@$this->motherLang?->name,
             'rating'=>$this->getRating(),
             'teaching_duration'=>null,
             'reviews'=>$this->reviews?collect(new TeacherReviewsCollection($this->reviews)):null
