108a240cc58c9cdab9a2c7282f0b6f62b96b3ae1
Mohamed Abbass <dev.3bbas@gmail.com>
2024-12-10 14:44:47 +0200

    comment un wanted

--- routes/web.php
+++ routes/web.php
@@ -269,7 +269,7 @@ Route::get('copy-subjects',function(){
             ->name ?? '';
 
             $category = Category::create( $request);
-            
+
             CategoryTranslation::create([
                 'name' =>  $request['name_ar'],
                 "locale" => 'ar',
@@ -288,70 +288,70 @@ Route::get('copy-subjects',function(){
 
 Route::get('/migrate',function(){
     $migrations = [
-        //session subscriptions
-        "2024_10_16_161429_add_price_column_to_course_sessions_table.php",
-        "2024_10_16_161505_add_price_column_to_course_sessions_group_table.php",
-        "2024_10_16_184609_create_course_session_subscriptions_table.php",
-        "2024_10_16_185019_add_can_subscribe_to_session_and_can_subscribe_to_session_group_columns_to_courses_table.php",
+        // //session subscriptions
+        // "2024_10_16_161429_add_price_column_to_course_sessions_table.php",
+        // "2024_10_16_161505_add_price_column_to_course_sessions_group_table.php",
+        // "2024_10_16_184609_create_course_session_subscriptions_table.php",
+        // "2024_10_16_185019_add_can_subscribe_to_session_and_can_subscribe_to_session_group_columns_to_courses_table.php",
 
-        //installments
-        "2024_10_20_150417_add_open_installment_column_to_courses_table.php",
-        "2024_10_20_165019_create_course_session_installments_table.php",
-        "2024_10_21_122451_create_student_session_installments_table.php",
+        // //installments
+        // "2024_10_20_150417_add_open_installment_column_to_courses_table.php",
+        // "2024_10_20_165019_create_course_session_installments_table.php",
+        // "2024_10_21_122451_create_student_session_installments_table.php",
 
-        //payments
-        "2024_10_23_194948_make_user_type_string_on_transactios_table.php",
-        "2024_10_24_135654_add_order_id_to_transactios_table.php",
+        // //payments
+        // "2024_10_23_194948_make_user_type_string_on_transactios_table.php",
+        // "2024_10_24_135654_add_order_id_to_transactios_table.php",
 
-        //quizes
-        "2024_10_28_215817_change_quiz_date_to_date_time_on_course_quizes_table.php",
+        // //quizes
+        // "2024_10_28_215817_change_quiz_date_to_date_time_on_course_quizes_table.php",
 
-        //assignments
-        "2024_10_28_215959_change_assignment_date_to_date_time_on_course_assignments_table.php",
-        "2024_10_30_011604_add_dob_column_to_join_as_teacher_requests_table.php",
+        // //assignments
+        // "2024_10_28_215959_change_assignment_date_to_date_time_on_course_assignments_table.php",
+        // "2024_10_30_011604_add_dob_column_to_join_as_teacher_requests_table.php",
 
-        //balances
-        "2024_10_31_124047_change_pay_transaction_id_to_string_on_balances_table.php",
+        // //balances
+        // "2024_10_31_124047_change_pay_transaction_id_to_string_on_balances_table.php",
 
-        //courses
-        "2024_10_31_163741_add_material_id_to_courses_table.php",
+        // //courses
+        // "2024_10_31_163741_add_material_id_to_courses_table.php",
 
-        //users
-        "2024_10_31_180444_add_material_id_to_users_table.php",
+        // //users
+        // "2024_10_31_180444_add_material_id_to_users_table.php",
 
-        //categories
-        "2024_11_01_233045_add_grade_sub_level_id_column_to_categories_table.php",
+        // //categories
+        // "2024_11_01_233045_add_grade_sub_level_id_column_to_categories_table.php",
 
-        //course_sessions
-        "2024_11_04_135359_add_meeting_id_to_course_sessions_table.php",
+        // //course_sessions
+        // "2024_11_04_135359_add_meeting_id_to_course_sessions_table.php",
 
-        //transactions
-        "2024_11_05_133910_change_brand_to_be_string_on_transactios_table.php",
+        // //transactions
+        // "2024_11_05_133910_change_brand_to_be_string_on_transactios_table.php",
 
-        //users
-        "2024_11_05_225728_add_session_token_to_users_table.php",
+        // //users
+        // "2024_11_05_225728_add_session_token_to_users_table.php",
 
-        "2024_11_07_002222_add_meeting_status_to_course_sessions_table.php",
+        // "2024_11_07_002222_add_meeting_status_to_course_sessions_table.php",
 
-        //quiz api
-        "2024_08_17_174646_create_course_quizzes_results_answers_table.php",
+        // //quiz api
+        // "2024_08_17_174646_create_course_quizzes_results_answers_table.php",
 
-        //assignment api
-        "2024_08_19_103701_create_course_assignments_results_answers_table.php",
+        // //assignment api
+        // "2024_08_19_103701_create_course_assignments_results_answers_table.php",
 
 
-        //favourtie
-        "2024_08_22_113909_create_favourites_table.php",
+        // //favourtie
+        // "2024_08_22_113909_create_favourites_table.php",
 
-        //add is installment to user course
-        "2024_11_24_030055_add_is_installment_to_user_courses_table.php",
+        // //add is installment to user course
+        // "2024_11_24_030055_add_is_installment_to_user_courses_table.php",
 
-        //chat
-        "2024_10_10_095450_add_read_at_to_chat_messages_table.php",
-        "2024_11_23_133757_change_course_lessons_storage_column_to_be_string.php",
-        "2024_12_04_135905_create_payment_details_table.php",
-        "2024_12_05_142018_add_is_refunded_column_to_transactios_table.php",
-        "2024_12_09_111058_add_is_feature_column_to_courses_table.php",
+        // //chat
+        // "2024_10_10_095450_add_read_at_to_chat_messages_table.php",
+        // "2024_11_23_133757_change_course_lessons_storage_column_to_be_string.php",
+        // "2024_12_04_135905_create_payment_details_table.php",
+        // "2024_12_05_142018_add_is_refunded_column_to_transactios_table.php",
+        // "2024_12_09_111058_add_is_feature_column_to_courses_table.php",
         "2024_12_10_115716_make_role_string_in_users_table.php",
         "2024_12_10_113824_create_parent_sons_table.php"
 
