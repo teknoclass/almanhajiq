a3b5f12b6805595db7ff3dc20c5a30fff0d27c0d
Rajaei003 <rajaei.alsafadi.2003@gmail.com>
2025-02-16 11:16:54 +0300

    test zain

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -242,7 +242,7 @@ class PaymentController extends Controller
                 $statusCheck = $this->zainCashService->checkPaymentStatus($paymentDetails['payment_id']);
                 if($statusCheck["status"] == "failed")
                 {
-                    return $statusCheck;
+                    //return $statusCheck;
                     return redirect('/payment-failure');
                 }
             }
